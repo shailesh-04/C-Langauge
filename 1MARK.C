7b637bae401ebecc80b1835562897dab16e6499f
shailesh-makavana <112607129+shailesh-makavana@users.noreply.github.com>
2022-09-01 11:08:20 +0530

    Add files via upload

---
+++ 1MARK.C
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+		int mark;
+
+		clrscr();
+		printf("enter mark :-");
+		scanf("%d",&mark);
+		if( mark<30)
+		{
+				printf("student pass");
+		}
+		 else
+		 {
+		  printf("student fail");
+		 }
+
+			getch();
+
+
+		}
