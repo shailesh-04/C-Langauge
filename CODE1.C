f3c6a8ee3988b274a65ae7e2e2ea9313ef526e0e
shailesh-makavana <112607129+shailesh-makavana@users.noreply.github.com>
2022-09-16 12:40:22 +0530

    Add files via upload

---
+++ CODE1.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+
+ {
+     int a;
+     int b;
+     int c;
+     clrscr();
+     printf("enter tow value");
+	  scanf("%d%d",&a,&b);
+	  c=a+b;
+	  printf("c is %d %d %d",a,b,c);
+     getch();
+     }
